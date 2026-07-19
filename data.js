9b5e7a8add703df4add75d935d6a49c0343fdec0
xphost008 <xphost@foxmail.com>
2026-07-19 17:09:37 +0800

    2

---
+++ data.js
@@ -0,0 +1,92 @@
+const schema = [
+  {
+    field: "clinic",
+    name: "专家门诊"
+  },
+  {
+    field: "name",
+    name: "专家"
+  },
+  {
+    field: "hospital",
+    name: "所属医院"
+  },
+  {
+    field: "job",
+    name: "职称"
+  },
+  {
+    field: "room",
+    name: "诊室"
+  },
+  {
+    field: "time",
+    name: "门诊时间"
+  }
+]
+const data = [
+  {
+    clinic: "名中医工作室",
+    name: "五要",
+    hospital: "上海市第六人民医院",
+    job: "主任亦是",
+    room: "一楼针腿上课们正201是",
+    time: [[7, 18, 1],[7, 25, 1]],
+  },
+  {
+    clinic: "名中医工作室",
+    name: "五要",
+    hospital: "上海市第六人民医院",
+    job: "主任亦是",
+    room: "一楼针腿上课们正201是",
+    time: [[7, 25, 1]]
+  },
+  {
+    clinic: "名中医工作室",
+    name: "五吃",
+    hospital: "上海市第六人民医院",
+    job: "主任亦是",
+    room: "一楼针腿上课们正201是",
+    time: [[7, 18, 0]]
+  },
+  {
+    clinic: "名中医工作室",
+    name: "要吃",
+    hospital: "上海市第六人民医院",
+    job: "主任亦是",
+    room: "一楼针腿上课们正201是",
+    time: [[7, 20, 1]]
+  },
+  {
+    clinic: "名中医工作室",
+    name: "五",
+    hospital: "上海市第六人民医院",
+    job: "主任亦是",
+    room: "一楼针腿上课们正201是",
+    time: [[7, 19, 0]]
+  },
+  {
+    clinic: "名中医工作室",
+    name: "要",
+    hospital: "上海市第六人民医院",
+    job: "主任亦是",
+    room: "一楼针腿上课们正201是",
+    time: [[7, 19, 1]]
+  },
+  {
+    clinic: "名中医工作室",
+    name: "吃",
+    hospital: "上海市第六人民医院",
+    job: "主任亦是",
+    room: "一楼针腿上课们正201是",
+    time: [[7, 20, 0]]
+  },
+  {
+    clinic: "名中医工作室",
+    name: "五要吃",
+    hospital: "上海市第六人民医院",
+    job: "主任亦是",
+    room: "一楼针腿上课们正201是",
+    time: [[7, 18, 1]]
+  },
+]
---
+++ index.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  root.innerHTML = `
+  <div style="display: flex; align-items: center; justify-content: space-around; background-color: rgb(223, 131, 68); border: 1px solid black;">
+    <h5 id="time" style="display: flex; align-items: center; justify-content: end;">
+    </h5>
+    <div style="display: flex; justify-content: center;">
+      <h3>专家门诊安排</h3>
+    </div>
+    <div style="display: flex; justify-content: start;">
+      <h4 style="margin: 0; padding: 0;">筛选：</h4><select id="select-grid">
+      </select>
+    </div>
+  </div>
+  <div id="content-grid">
+  </div>`
+  const time = document.querySelector("#time")
+  const date = new Date()
+  console.log(data.flatMap(obj => obj.time));
+  time.innerHTML = `当前日期：${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+  const select = document.querySelector("#select-grid")
+  select.innerHTML = `<option value="day">当天</option>${data.flatMap((d) => {
+    return d.time
+  }).filter((d1, i, s) => {
+    return s.findIndex((d2) => {
+      return d1[1] === d2[1]
+    }) === i
+  }).sort((a, b) => {
+    return a[1] - b[1]
+  }).map((d) => {
+    return `<option value="${d.join(',')}">${d[1]}日</option>`
+  }).join("")}`
+  select.addEventListener("change", onSelectChangeEvent)
+  onSelectChangeEvent({target: {value: "day"}})
+})
+function onSelectChangeEvent(event) {
+  const grid = document.querySelector("#content-grid")
+  grid.innerHTML = schema.map((item, index) => {
+    return `<div class="p fb center tl tb ${index === schema.length - 1 ? "tr" : ""}">${item.name}</div>`
+  }).join("");
+  let value = event.target.value
+  if (value === "day") {
+    const date = new Date()
+    value = `${date.getMonth() + 1},${date.getDate()},0`
+  }
+  console.log(value)
+  const split = value.split(',')
+  const filtered = data.filter((d) => d.time.some(s => s[1] === parseInt(split[1])))
+  grid.innerHTML += filtered.map((d) => {
+    return schema.map((s, index) => {
+      return `<div class="p center tl tb ${index === schema.length - 1 ? "tr" : ""}">${s.field === "time" ? (function (times) {
+        return times.map((time) => {
+          return `${time[0]}月${time[1]}日${time[2] == 0 ? '上午' : '下午'}`
+        }).join(',')
+      })(d[s.field]) : d[s.field]}</div>`
+    }).join("")
+  }).join("")
+}
